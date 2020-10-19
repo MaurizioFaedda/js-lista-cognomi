503396abb2a5b93ebdd27b6e300bfc3069f79b66
Maurizio <maurizio.faedda1@gmail.com>
2020-10-19 14:32:04 +0200

    Versione semplice completata

---
+++ main.js
@@ -0,0 +1,27 @@
+// Creare un array contenente dei cognomi in ordine casuale.
+// Chiedere all'utente il suo cognome e inserirlo nella lista dei cognomi.
+// Stampare la lista dei cognomi in ordine alfabetico e comunicare all'utente la posizione in cui si trova il suo cognome (dopo averlo riordinato).
+
+// Creo il mio Array in ordine causale
+var cognomi = ["Rossi", "Verdi", "Ricci", "Bianchi", "Ferrari"];
+
+// L'utente inserisce il suo cognome
+var cognomeUtente = prompt("Inserirsci il tuo cognome");
+
+// Inserisco il cognome dell'utente dentro l'array
+cognomi.push(cognomeUtente);
+
+// stampa di tutti i cognomi in ordine casuale
+console.log(cognomi);
+
+// Ordino i cognomi
+cognomi.sort();
+
+// Stampo tutti i cognomi in ordine alfabetico
+console.log(cognomi);
+
+// Dichiaro la mia variabile per indicare la posizione
+var posizione = cognomi.indexOf(cognomeUtente) + 1;
+
+// Stampo la posizione dell'utente
+console.log("L'utente " + cognomeUtente + " è alla " + posizione + "° posizione");
